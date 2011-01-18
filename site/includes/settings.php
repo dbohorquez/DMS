3effcd84cb4118d654c41f17a2f758c3554ec587
David Bohorquez <david@coddeeweb.com>
2011-01-18 00:18:43 -0500

    base del portal web

---
+++ site/includes/settings.php
@@ -0,0 +1,11 @@
+<?php
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '1234';	
+$dbDatabase = '';
+
+date_default_timezone_set("America/Bogota");
+setlocale(LC_TIME,"spanish","es_CO");
+
+/*error_reporting(0);*/
+?>
